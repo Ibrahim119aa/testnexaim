65d0b4aaaebc43ba694d62f8dcb0660325f09cd3
Abbas-Imran <balochabbas241@gmail.com>
2025-03-26 03:15:14 +0500

    chore: added google pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,43 @@
 import React from "react";
 import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
-
-// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Nexaim",
   description: "Nexaim is a digital marketing agency that helps businesses grow online.",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      {/* <head></head> */}
+      <body>
+        {children}
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2052579801877656');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2052579801877656&ev=PageView&noscript=1"
+            alt="fb-pixel"
+          />
+        </noscript>
+      </body>
     </html>
   );
 }
