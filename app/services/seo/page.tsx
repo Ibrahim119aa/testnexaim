49b367242c7e416a1554a0ee04c297b9265ac872
Ibrahim119aa <ibrahimmemon1709@gmail.com>
2025-07-18 02:15:32 +0500

    chore:inspelling and logo align

--- app/services/seo/page.tsx
+++ app/services/seo/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
     image: "/assets/SEO.jpg",
     highlight: "Pakistan!",
     paragraphs: [
-      "We’re not just another SEO company in Pakistan — we’re your growth partners. At Nexiom, we specialize in turning search traffic into real business by combining data-driven strategies with personalized SEO solutions. Whether you're a local startup or an international brand targeting Pakistan, our job is to make sure your customers find you before your competitors.",
+      "We’re not just another SEO company in Pakistan — we’re your growth partners. At Nexaim, we specialize in turning search traffic into real business by combining data-driven strategies with personalized SEO solutions. Whether you're a local startup or an international brand targeting Pakistan, our job is to make sure your customers find you before your competitors.",
       "With years of experience, a proven track record, and a passion for digital performance, we take your online visibility seriously — so you don’t have to."
 
     ],
