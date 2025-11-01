3cd1942ee62781c8c1be19e55eddac94e858e390
Ibrahim <139770979+Ibrahim119aa@users.noreply.github.com>
2025-11-01 03:23:12 -0700

    upload after testing website development page

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
   return (
     <>
+      <head>
+        <meta name="google-site-verification" content="rSmgiUi5hClwP2sfbwSV1xX0POJwTuw5IjXRyxhtECs" />
+      </head>
       {children}
     </>
   );
